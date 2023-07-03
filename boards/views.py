892f373b2e452e6d838d58ea4a6fbc13037df3c9
abdul-sam <abdulsamad.3939@gmail.com>
2023-07-03 18:13:40 +0500

    Completed User Authentication

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from django.contrib.auth.models import User
@@ -15,20 +16,20 @@ def board_topics(request, pk):
 		raise Http404
 	return render(request, 'topics.html', { 'board': board })
 
+@login_required
 def new_topic(request, pk):
 	board = get_object_or_404(Board, pk=pk)
-	user = User.objects.first()
 	if request.method == 'POST':
 		form = NewTopicForm(request.POST)
 		if form.is_valid():
 			topic = form.save(commit=False)
 			topic.board = board
-			topic.starter = user
+			topic.starter = request.user
 			topic.save()
 			Post.objects.create(
         message=form.cleaned_data.get('message'),
         topic=topic,
-        created_by=user
+        created_by=request.user
       )
 			return redirect('board_topics', pk=board.pk)
 	else:
--- myproject/urls.py
+++ myproject/urls.py
@@ -31,4 +31,26 @@ urlpatterns = [
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     # url(r'^(?P<username>[\w.@+-]+)/$', views.user_profile, name='user_profile'), This will match all valid usernames for a Django User model.
     path('admin/', admin.site.urls),
+
+    re_path(r'^reset/$',
+            auth_views.PasswordResetView.as_view(
+                template_name='password_reset.html',
+                email_template_name='password_reset_email.html',
+                subject_template_name='password_reset_subject.txt'),
+            name='password_reset'),
+    re_path(r'^reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    re_path(r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+    re_path(r'^settings/password/$',
+        auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    re_path(r'^settings/password/done/$',
+        auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),
 ]
